588cda2ab624cf641b6e8a482262874059183ed5
André Portes Ferriani <andre.portes17@gmail.com>
2021-06-20 22:09:40 -0300

    Initial Commit

---
+++ Laboratorio_01/src/Laboratorio_01.c
@@ -0,0 +1,45 @@
+#include <stdint.h>
+#include <stdbool.h>
+// includes da biblioteca driverlib
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+
+uint8_t LED_D1 = 0;
+uint8_t LED_D4 = 0;
+
+void SysTick_Handler(void){
+    //
+} // SysTick_Handler
+
+void main(void){
+  SysTickPeriodSet(12000000); // f = 1Hz para clock = 24MHz
+  
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Habilita GPIO F (LED D4 = PF0)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); // Aguarda final da habilitação
+    
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0); // LED D4 como saída
+  GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0); // LEDs D4 apagados
+  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
+    
+  SysTickIntEnable();
+  SysTickEnable();
+  
+  float counter = 0;
+  float frequencia = SysTickValueGet();
+  float counterlimit = frequencia*1;
+  
+  while(1){
+    if(counter >= counterlimit)
+    {
+      LED_D4 ^= GPIO_PIN_0; // Troca estado do LED D1
+      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, LED_D4);
+      counter = 0;
+    }
+    else
+    {
+      counter++;
+    }
+  } // while
+} // main
